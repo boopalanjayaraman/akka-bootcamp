5120bd18523f7c33e51b68489941d2c71762b92e
boopalan.j <boopalan.j@payoda.com>
2017-06-23 12:48:58 +0530

    Finished Unit1 - lesson1 and lesson2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,7 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,21 +20,64 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
+            {
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
+            }
+
+            GetAndValidateInput();
+        }
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(message))
+            {
+                Self.Tell(new Messages.InputError("No input received."));
+            }
+            else if (!string.IsNullOrEmpty(message) && String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
             {
                 // shut down the system (acquire handle to system via
                 // this actors context)
                 Context.System.Terminate();
                 return;
             }
+            else
+            {
+                bool valid = IsValid(message);
+                if(valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you. Message was success."));
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
+            //// send input to the console writer to process and print
+            //_consoleWriterActor.Tell(read);
 
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            //// continue reading messages from the console
+            //Self.Tell("continue");
         }
 
+        private bool IsValid(string message)
+        {
+            var even = message.Length % 2 == 0;
+            return even;
+        }
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console.");
+            Console.WriteLine("Some entries will pass validation, some wont..\n\n");
+            Console.WriteLine("Type 'exit' to quit this app any time. \n");
+        }
     }
 }
---
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTail
+{
+    public class ValidationActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+
+        public ValidationActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsValid(string message)
+        {
+            var even = message.Length % 2 == 0;
+            return even;
+        }
+    }
+}
